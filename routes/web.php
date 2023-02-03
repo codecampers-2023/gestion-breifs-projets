3c1d53c2ff1cbbd432400f7b213999352d1d9103
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-03 04:12:40 -0800

    authorisation

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view("dashboard");
+    }) ->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
