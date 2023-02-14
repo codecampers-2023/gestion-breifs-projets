26cd7512c5202e8daa6458a0a520ddf99d792e07
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-14 14:58:02 +0100

    added DB/Migration.

--- database/migrations/2022_12_27_081850_create_apprenant_table.php
+++ database/migrations/2022_12_27_081850_create_apprenant_table.php
@@ -23,8 +23,39 @@ return new class extends Migration
             $table->string("CIN")->nullable();
             $table->date("Date_naissance")->nullable();
             $table->string("Image")->nullable();
+            // 
+            $table->string("sexe")->nullable();
+            $table->string("Etudiant_actif")->nullable();
+            $table->string("Diplome")->nullable();
+            $table->Date("Date_inscription")->nullable();
+            $table->string("Lieu_naissance")->nullable();
+            $table->string("sexe")->nullable();
+            $table->string("Etudiant_actif")->nullable();
+            $table->string("Diplome")->nullable();
+            $table->Date("Date_inscription")->nullable();
+            $table->string("Lieu_naissance")->nullable();
+            $table->integer("Numero_telephone")->nullable(); //test.
+            $table->string("Nom_arabe")->nullable();
+            $table->string("Prenom_arabe")->nullable();
+            $table->string("Niveau_Scoliare")->nullable();
+            $table->string("Nom_arabe")->nullable();
+            $table->string("Prenom_arabe")->nullable();
+            $table->string("Niveau_Scoliare")->nullable();
         });
+
+        Schema::create('annee_formation', function (Blueprint $table) {
+            
+         $table->increments('id');
+         $table->date('date_debut');
+         $table->date('date_fin');
+         $table->string('ref'); // adnane : To verify if column type should be Year?
+        });
+
+
+
     }
+
+    
 
     /**
      * Reverse the migrations.
