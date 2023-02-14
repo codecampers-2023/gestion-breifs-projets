4911bb2842de13762a96cb27d4de288d7dc7fbe1
bilalbarina <dev.barina@gmail.com>
2023-02-14 15:13:20 +0100

    Fixed buges

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,10 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 Route::middleware('auth')->group(function () {
+    Route::view('/', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/dashboard', function () {
-        return view("dashboard");
-    }) ->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
